f48a21933fb2259e21ba0516708495a09d20d8bc
Pedro Gomes <pedrogomes94@gmail.com>
2019-07-12 16:31:42 +0100

    verify balances

--- src/helpers/transaction.ts
+++ src/helpers/transaction.ts
@@ -1,6 +1,17 @@
 import { convertNumberToHex, convertUtf8ToNumber } from "@walletconnect/utils";
-import { convertAmountToRawNumber } from "../helpers/bignumber";
-import { apiGetAccountNonce, apiGetGasPrices } from "../helpers/api";
+import {
+  convertAmountToRawNumber,
+  multiply,
+  add,
+  greaterThanOrEqual,
+  smallerThan
+} from "../helpers/bignumber";
+import {
+  apiGetAccountNonce,
+  apiGetGasPrices,
+  apiGetAccountBalance,
+  apiGetTokenBalance
+} from "../helpers/api";
 import {
   getDataString,
   removeHexPrefix,
@@ -16,18 +27,34 @@ export async function formatTransaction(
   amount: string,
   currency: string
 ) {
+  const chainId = 1;
+
   let value = "";
   let data = "";
   let gasLimit = 0;
 
   amount = convertAmountToRawNumber(amount);
 
+  const nonce = await apiGetAccountNonce(from, chainId);
+
+  const gasPrices = await apiGetGasPrices();
+  const gasPrice = convertUtf8ToNumber(
+    convertAmountToRawNumber(gasPrices.average.price, 9)
+  );
+
+  const eth = await apiGetAccountBalance(from, chainId);
   if (currency && currency.toUpperCase() === "ETH") {
     value = amount;
     data = "0x";
     gasLimit = 21000;
+    const gasTotal = multiply(gasPrice, gasLimit);
+    const total = add(amount, gasTotal);
+    if (smallerThan(eth.balance || "0", total)) {
+      throw new Error("ETH balance is not enough");
+    }
   } else if (currency && currency.toUpperCase() === "DAI") {
     const tokenAddress = DAI_TOKEN_ADDRESS;
+    const dai = await apiGetTokenBalance(from, tokenAddress, chainId);
     value = "0x00";
     data = getDataString(TOKEN_TRANSFER, [
       removeHexPrefix(to),
@@ -35,16 +62,16 @@ export async function formatTransaction(
     ]);
     gasLimit = 40000;
     to = tokenAddress;
+    const gasTotal = multiply(gasPrice, gasLimit);
+    if (smallerThan(dai.balance || "0", amount)) {
+      throw new Error("DAI balance is not enough");
+    } else if (smallerThan(eth.balance || "0", gasTotal)) {
+      throw new Error("Not enough ETH to cover gas costs");
+    }
   } else {
     throw new Error(`Currency ${currency} not supported!`);
   }
 
-  const nonce = await apiGetAccountNonce(from, 1);
-
-  const gasPrices = await apiGetGasPrices();
-  const gasPrice = convertUtf8ToNumber(
-    convertAmountToRawNumber(gasPrices.average.price, 9)
-  );
   const tx = {
     from: sanitizeHex(from),
     to: sanitizeHex(to),
